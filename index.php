419a0f7e2b054bc20c2b7c98c9eb94c3a7e848f7
Alister Huysmans <alister.huysmans@gmail.com>
2023-06-05 20:11:41 +0200

    added index.php & contact.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chalet - Home</title>
+    <link rel="stylesheet" href="./styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/ff4f8ab1ed.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <nav>
+            <a class="logo" href="#">
+                <img src="" alt="Logo">
+            </a>
+            <ul>
+                <li>
+                    <a class="nav-link" href="./index.php">Accueil</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="./shop.php">Nos Chalets</a>
+                </li>
+                <li>
+                    <a class="nav-link" href="./contact.php">Contact</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="https://www.linkedin.com/company/chalet-nursery/">
+                        <i class="fa-brands fa-lg fa-linkedin"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/groups/546749599041947">
+                        <i class="fa-brands fa-lg fa-facebook"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <section class="hero-section">
+
+    </section>
+    <footer>
+
+    </footer>
+</body>
+</html>
