1f53267376dca97b8caa1f2bbb47bf629ca9ef14
Alister Huysmans <alister.huysmans@gmail.com>
2023-06-05 23:56:11 +0200

    yes

--- view/header.php
+++ view/header.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chalet - Home</title>
+    <title>Chalets de Rêve</title>
     <link rel="stylesheet" href="../styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Oswald:wght@700&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/ff4f8ab1ed.js" crossorigin="anonymous"></script>
+    <link rel="apple-touch-icon" sizes="180x180" href="../favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../favicon/favicon-16x16.png">
+    <link rel="manifest" href="../favicon/site.webmanifest">
 </head>
 <body>
     <header>
--- view/index.php
+++ view/index.php
@@ -4,7 +4,8 @@
     <section id="home">
         <div class="hero-section">
             <h1>Découvrez nos chalets somptueux</h1>
-            <p>Bienvenue chez ChaletRêve, votre destination ultime pour des vacances en chalets de luxe. Plongez-vous dans la beauté naturelle de nos emplacements idylliques et détendez-vous dans un cadre enchanteur. Que vous souhaitiez skier en hiver ou vous détendre près d'un feu de cheminée, nos chalets offrent le cadre idéal pour des moments inoubliables en famille ou entre amis. Réservez dès maintenant et vivez des moments magiques dans nos chalets de rêve.</p>
+            <p class="hero-text-1">Bienvenue chez Chalets de Rêve, votre destination ultime pour des vacances en chalets de luxe.</p>
+            <p class="hero-text-2">Plongez-vous dans la beauté naturelle de nos emplacements idylliques et détendez-vous dans un cadre enchanteur. Que vous souhaitiez skier en hiver ou vous détendre près d'un feu de cheminée, nos chalets offrent le cadre idéal pour des moments inoubliables en famille ou entre amis. Réservez dès maintenant et vivez des moments magiques dans nos chalets de rêve.</p>
             <a href="./shop.php" class="btn-home">Découvrez nos chalets</a>
         </div>
     </section>
